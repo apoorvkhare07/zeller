235473c6344a5c72c10af638631c777b319c98fd
apoorvkhare <apoorv.khare@vegapay.tech>
2023-04-28 15:52:14 +0530

    add camera & film model to inventory

--- zeller/core_site/models.py
+++ zeller/core_site/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class AbstractBase(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
-    modified_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name="user_created_%(class)s_related")
+    modified_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT, related_name="user_modified_%(class)s_related")
 
     class Meta:
         abstract = True
