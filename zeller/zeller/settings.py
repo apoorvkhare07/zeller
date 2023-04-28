c7f47aeb29b48420c73b65c65bd4d5016482c836
apoorvkhare07 <apoorvkhare007@gmail.com>
2023-04-28 18:49:41 +0530

    add sales app

--- zeller/zeller/settings.py
+++ zeller/zeller/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'core_site',
     'inventory',
+    'sales',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
